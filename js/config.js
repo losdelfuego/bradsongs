65d8d55eb7cc418bb1923397925a3dc9b3ada0ec
Carlos Lamas <losrocks@gmail.com>
2024-01-23 13:13:14 -0600

    airtable api update

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 var config = {
-  airtablekey : 'keyZ5ixWh8VGnbyu6',
+  airtablekey : 'patCs1veReEJPZ4vR.07fe2b3ceae4ec040302c98f6541d5bbe4f5dd9d15bd8847a651bc8dfda33d9f',
   dbkey : 'appL3FuQ8BfpbKcsB',
 }
